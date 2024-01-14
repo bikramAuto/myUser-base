25b2ae180879127a937e1fb47d4a0c964b0002d8
Bikram <bikram.nanda@squbix.com>
2024-01-14 13:08:06 +0530

    1st commit

---
+++ src/main/java/com/userbase/user/UserBaseApplication.java
@@ -0,0 +1,13 @@
+package com.userbase.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserBaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/userbase/user/UserBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.userbase.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
