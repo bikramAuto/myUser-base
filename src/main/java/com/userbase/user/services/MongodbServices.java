2c9003a4919b6d235e7c19686133c41756ec284d
Bikram <bikram.nanda@squbix.com>
2024-01-14 17:51:14 +0530

    1st commit

--- src/main/java/com/userbase/user/services/MongodbServices.java
+++ src/main/java/com/userbase/user/services/MongodbServices.java
@@ -1,8 +1,12 @@
 package com.userbase.user.services;
 
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 public class MongodbServices {
@@ -10,5 +14,11 @@ public class MongodbServices {
     @Autowired
     private MongoTemplate mongoTemplate;
 
+    public void create(String colletionName, Map<String, Object> document){
+        MongoCollection<Document> collection = mongoTemplate.getCollection(colletionName);
+        Document dbDocument = new Document(document);
+        collection.insertOne(dbDocument);
+        System.out.println("document insert successfully");
+    }
 
 }
